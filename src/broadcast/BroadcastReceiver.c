3eeeeb2f2538ccc62386520a62525b61e738ccd5
Mustakimur Rahman <mustakcsecuet@gmail.com>
2025-03-30 22:31:17 -0400

    broadcast message

--- src/broadcast/BroadcastReceiver.c
+++ src/broadcast/BroadcastReceiver.c
@@ -5,47 +5,52 @@
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
 
-#define MAXRECVSTRING 255  /* Longest string to receive */
+#define MAXRECVSTRING 255 /* Longest string to receive */
 
-void DieWithError(char *errorMessage);  /* External error handling function */
+void DieWithError(char *errorMessage) {
+  perror(errorMessage);
+  exit(1);
+}
 
-int main(int argc, char *argv[])
-{
-    int sock;                         /* Socket */
-    struct sockaddr_in broadcastAddr; /* Broadcast Address */
-    unsigned short broadcastPort;     /* Port */
-    char recvString[MAXRECVSTRING+1]; /* Buffer for received string */
-    int recvStringLen;                /* Length of received string */
+int main(int argc, char *argv[]) {
+  int sock;                           /* Socket */
+  struct sockaddr_in broadcastAddr;   /* Broadcast Address */
+  unsigned short broadcastPort;       /* Port */
+  char recvString[MAXRECVSTRING + 1]; /* Buffer for received string */
+  int recvStringLen;                  /* Length of received string */
 
-    if (argc != 2)    /* Test for correct number of arguments */
-    {
-        fprintf(stderr,"Usage: %s <Broadcast Port>\n", argv[0]);
-        exit(1);
-    }
+  if (argc != 2) /* Test for correct number of arguments */
+  {
+    fprintf(stderr, "Usage: %s <Broadcast Port>\n", argv[0]);
+    // /BroadcastReceiver 3300
+    exit(1);
+  }
 
-    broadcastPort = atoi(argv[1]);   /* First arg: broadcast port */
+  broadcastPort = atoi(argv[1]); /* First arg: broadcast port */
 
-    /* Create a best-effort datagram socket using UDP */
-    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-        DieWithError("socket() failed");
+  /* Create a best-effort datagram socket using UDP */
+  if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+    DieWithError("socket() failed");
 
-    /* Construct bind structure */
-    memset(&broadcastAddr, 0, sizeof(broadcastAddr));   /* Zero out structure */
-    broadcastAddr.sin_family = AF_INET;                 /* Internet address family */
-    broadcastAddr.sin_addr.s_addr = htonl(INADDR_ANY);  /* Any incoming interface */
-    broadcastAddr.sin_port = htons(broadcastPort);      /* Broadcast port */
+  /* Construct bind structure */
+  memset(&broadcastAddr, 0, sizeof(broadcastAddr)); /* Zero out structure */
+  broadcastAddr.sin_family = AF_INET; /* Internet address family */
+  broadcastAddr.sin_addr.s_addr =
+      htonl(INADDR_ANY);                         /* Any incoming interface */
+  broadcastAddr.sin_port = htons(broadcastPort); /* Broadcast port */
 
-    /* Bind to the broadcast port */
-    if (bind(sock, (struct sockaddr *) &broadcastAddr, sizeof(broadcastAddr)) < 0)
-        DieWithError("bind() failed");
+  /* Bind to the broadcast port */
+  if (bind(sock, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr)) < 0)
+    DieWithError("bind() failed");
 
-    /* Receive a single datagram from the server */
-    if ((recvStringLen = recvfrom(sock, recvString, MAXRECVSTRING, 0, NULL, 0)) < 0)
-        DieWithError("recvfrom() failed");
+  /* Receive a single datagram from the server */
+  if ((recvStringLen = recvfrom(sock, recvString, MAXRECVSTRING, 0, NULL, 0)) <
+      0)
+    DieWithError("recvfrom() failed");
 
-    recvString[recvStringLen] = '\0';
-    printf("Received: %s\n", recvString);    /* Print the received string */
-    
-    close(sock);
-    exit(0);
+  recvString[recvStringLen] = '\0';
+  printf("Received: %s\n", recvString); /* Print the received string */
+
+  close(sock);
+  exit(0);
 }
